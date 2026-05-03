dbaaf6df7db0b3ffb95910c3ebe9892f8b3fa92b
Atul Tameshwari <atameshwari9@gmail.com>
2026-05-03 11:29:29 +0530

    Add constants.js file with sample story data

---
+++ constants.js
@@ -0,0 +1,72 @@
+const stories = [
+    {
+      id: 1,
+      username: "alexrivera",
+      profilePic: "https://randomuser.me/api/portraits/men/32.jpg",
+      timeAgo: "2h",
+      storyImage: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee"
+    },
+    {
+      id: 2,
+      username: "sophia.k",
+      profilePic: "https://randomuser.me/api/portraits/women/44.jpg",
+      timeAgo: "5h",
+      storyImage: "https://images.unsplash.com/photo-1492724441997-5dc865305da7"
+    },
+    {
+      id: 3,
+      username: "jayden_lee",
+      profilePic: "https://randomuser.me/api/portraits/men/76.jpg",
+      timeAgo: "1h",
+      storyImage: "https://images.unsplash.com/photo-1519681393784-d120267933ba"
+    },
+    {
+      id: 4,
+      username: "emma.wat",
+      profilePic: "https://randomuser.me/api/portraits/women/68.jpg",
+      timeAgo: "30m",
+      storyImage: "https://images.unsplash.com/photo-1506744038136-46273834b3fb"
+    },
+    {
+      id: 5,
+      username: "noah.dev",
+      profilePic: "https://randomuser.me/api/portraits/men/12.jpg",
+      timeAgo: "8h",
+      storyImage: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e"
+    },
+    {
+      id: 6,
+      username: "mia_s",
+      profilePic: "https://randomuser.me/api/portraits/women/21.jpg",
+      timeAgo: "3h",
+      storyImage: "https://images.unsplash.com/photo-1517841905240-472988babdf9"
+    }, 
+    {
+        id: 7,
+        username: "lonewolf",
+        profilePic: "https://randomuser.me/api/portraits/men/9.jpg",
+        timeAgo: "3h",
+        storyImage: "https://images.unsplash.com/photo-1517841905240-472988babdf9"
+    },
+    {
+        id: 8,
+        username: "the_boss",
+        profilePic: "https://randomuser.me/api/portraits/men/11.jpg",
+        timeAgo: "3h",
+        storyImage: "https://images.unsplash.com/photo-1517841905240-472988babdf9"
+    },
+    {
+        id: 9,
+        username: "the_boss",
+        profilePic: "https://randomuser.me/api/portraits/men/19.jpg",
+        timeAgo: "3h",
+        storyImage: "https://images.unsplash.com/photo-1517841905240-472988babdf9"
+    },
+    {
+        id: 10,
+        username: "the_boss",
+        profilePic: "https://randomuser.me/api/portraits/men/13.jpg",
+        timeAgo: "3h",
+        storyImage: "https://images.unsplash.com/photo-1517841905240-472988babdf9"
+    }
+];
