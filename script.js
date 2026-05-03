4f78a12206a4dc0c581440a55b789a1a171af638
Atul Tameshwari <atameshwari9@gmail.com>
2026-05-03 12:20:40 +0530

    Add story header and footer components to index.html; update script.js to display user info and story interactions; enhance styling in style.css for new elements.

--- script.js
+++ script.js
@@ -3,58 +3,65 @@ import { stories } from "./constants.js";
 const storyImg = document.querySelector(".story-img");
 const leftZone = document.querySelector(".nav.left");
 const rightZone = document.querySelector(".nav.right");
+const profilePic = document.querySelector(".profile-pic");
+const usernameEl = document.querySelector(".username");
+const timeEl = document.querySelector(".time");
 
 let currentIndex = 0;
 let timer = null;
 
 const showStory = () => {
-  const story = stories[currentIndex];
+    const story = stories[currentIndex];
 
-  storyImg.style.opacity = "0";
+    storyImg.style.opacity = "0";
 
-  const img = new Image();
-  img.src = story.storyImage;
+    const img = new Image();
+    img.src = story.storyImage;
 
-  img.onload = () => {
+    img.onload = () => {
     storyImg.src = story.storyImage;
     storyImg.style.opacity = "1";
-  };
+    };
+
+    profilePic.src = story.profilePic;
+    usernameEl.textContent = story.username;
+    timeEl.textContent = story.timeAgo;
 };
 
 const nextStory = () => {
-  if (currentIndex < stories.length - 1) {
-    currentIndex++;
-  } else {
-    currentIndex = 0;
-  }
+    if (currentIndex < stories.length - 1) {
+        currentIndex++;
+    } else {
+        currentIndex = 0;
+    }
 
-  showStory();
-  resetTimer();
+    showStory();
+    resetTimer();
 };
 
 const prevStory = () => {
-  if (currentIndex > 0) {
-    currentIndex--;
-  } else {
-    currentIndex = stories.length - 1;
-  }
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = stories.length - 1;
+    }
 
-  showStory();
-  resetTimer();
+    showStory();
+    resetTimer();
 };
 
 const startTimer = () => {
-  timer = setInterval(nextStory, 5000);
+    timer = setInterval(nextStory, 5000);
 };
 
 const resetTimer = () => {
-  clearInterval(timer);
-  startTimer();
+    clearInterval(timer);
+    startTimer();
 };
 
 const init = () => {
-  showStory();
-  startTimer();
+    showStory();
+    startTimer();
 };
 
 init();
